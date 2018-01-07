7aacf31fff8b83258a02226cd76f8d0b430b855b
langker <langker@aliyun.com>
2018-01-07 15:33:58 +0100

    add unrolling for version

--- unit_test.c
+++ unit_test.c
@@ -4,29 +4,37 @@
 int main(int argc, char *argv[]) {
 	
 	double *rightdata= process_withoutomp();
-	double *testdata1= process_withomp1();
-	double *testdata= process_withomp();
+	double *testdatausingtask= process_withompusingtask();
+	double *testdatausingunrollingfor= process_withompusingunrollingfor();
+	double *testdatausingfor= process_withompusingfor();
 	
 	for(int i = 0; i < GRID_SZ; i++){
 		for(int j = 0; j < GRID_SZ; j++){
-			if (rightdata[i*GRID_SZ+j]!=testdata1[i*GRID_SZ+j]) {
-				printf("wrong1");
+			if (rightdata[i*GRID_SZ+j]!=testdatausingtask[i*GRID_SZ+j]) {
+				printf("wrong task");
 				return 0;
 			}
 		}
 	}
 	for(int i = 0; i < GRID_SZ; i++){
 		for(int j = 0; j < GRID_SZ; j++){
-			if (rightdata[i*GRID_SZ+j]!=testdata[i*GRID_SZ+j]) {
-				printf("wrong");
+			if (rightdata[i*GRID_SZ+j]!=testdatausingunrollingfor[i*GRID_SZ+j]) {
+				printf("wrong unrollingfor");
+				return 0;
+			}
+		}
+	}
+	for(int i = 0; i < GRID_SZ; i++){
+		for(int j = 0; j < GRID_SZ; j++){
+			if (rightdata[i*GRID_SZ+j]!=testdatausingfor[i*GRID_SZ+j]) {
+				printf("wrong for");
 				return 0;
 			}
 		}
 	}
 	printf("no wrong\n");
 }
-// WITHOMP PART
-double *process_withomp1() {
+double *process_withompusingtask() {
 		double start = omp_get_wtime();
 		
 		int i, j;
@@ -68,8 +76,9 @@ double *process_withomp1() {
 		
 		#pragma omp barrier
 		
-		for(i = 0; i < 20; i++) {
-			sequential_update_withomp1( data, olddata, newdata, C, K, dt);
+		
+		for(i = 0; i < TIMES; i++) {
+			sequential_update_withompusingtask( data, olddata, newdata, C, K, dt);
 			tmp = olddata;
 			olddata = data;
 			data = newdata;
@@ -81,7 +90,7 @@ double *process_withomp1() {
 		printf("with omp spend: %f\n",end-start);
 		return data;
 }
-void sequential_update_withomp1(double *data, double *olddata, double *newdata, double C, double K, double dt ) {
+void sequential_update_withompusingtask(double *data, double *olddata, double *newdata, double C, double K, double dt ) {
 		int i, j;
 		double pot;
 		#pragma omp parallel private(i,j,pot)
@@ -102,10 +111,87 @@ void sequential_update_withomp1(double *data, double *olddata, double *newdata, 
 			}
 		}
 }
+// WITHOMP PART
+double *process_withompusingunrollingfor() {
+		double start = omp_get_wtime();
+		
+		int i, j;
+		double dt = 0.04, C = 16, K = 0.1, h = 6;
+		double *data, *olddata, *newdata, *tmp;
+		double x[PEAK_SZ][PEAK_SZ], linspace[PEAK_SZ], delta = 2.0/(PEAK_SZ-1.0);
+		data = (double*)malloc(sizeof(double)*ARR_SZ);
+		olddata = (double*)malloc(sizeof(double)*ARR_SZ);
+		newdata = (double*)malloc(sizeof(double)*ARR_SZ);
+		
+		#pragma omp parallel for private(i) schedule(auto)
+		for(i = 0; i < ARR_SZ; i++){
+				data[i] = 1.0;
+		}
+		
+		#pragma omp parallel for private(i,j) schedule(auto)
+		for(i = 0; i < PEAK_SZ; i++){
+			linspace[i] = -1.0 + delta * i;
+			for(j = 0; j < PEAK_SZ; j++) {
+				x[i][j] = linspace[i];
+			}
+		}
+		
+		#pragma omp barrier
+		
+		#pragma omp parallel for private(i,j) schedule(auto)
+		for(i = 0; i < PEAK_SZ; i++){
+			for(j = 0; j < PEAK_SZ; j++){
+				data[(i+20)*GRID_SZ+j+20] += h * exp( -5 * (pow(x[i][j], 2 ) + pow(x[j][i], 2 )));
+			}
+		}
+		
+		#pragma omp barrier
+		
+		#pragma omp parallel for private(i) schedule(auto)
+		for(i = 0; i < ARR_SZ; i++){
+			olddata[i] = data[i];
+		}
+		
+		#pragma omp barrier
+		
+		#pragma omp parallel for ordered
+		for(i = 0; i < TIMES/3; i++) {
+			#pragma omp ordered
+			{
+				sequential_update_withompusingunrollingfor( data, olddata,  newdata, C, K, dt);
+				sequential_update_withompusingunrollingfor( newdata, data , olddata, C, K, dt);
+				sequential_update_withompusingunrollingfor( olddata, newdata,  data, C, K, dt);
+			}
+		}
+		
+		
+		double end = omp_get_wtime();
+		printf("with omp spend: %f\n",end-start);
+		return data;
+}
+void sequential_update_withompusingunrollingfor(double *data, double *olddata, double *newdata, double C, double K, double dt ) {
+		int i, j;
+		double pot;
+		for( i = 0; i < GRID_SZ; i++) {
+			for( j = 0; j < GRID_SZ; j++) {
+				{
+				pot = data[(i+1 >= GRID_SZ ? i : i+1)*GRID_SZ+j]+
+						data[(i-1 < 0 ? 0 : i-1)*GRID_SZ+j]+
+						data[(j+1 >= GRID_SZ ? j : j+1)+i*GRID_SZ]+
+						data[(j-1 < 0 ? 0 : j-1)+i*GRID_SZ]
+						-4*data[i*GRID_SZ+j];
+				newdata[i * GRID_SZ + j] = 
+								(pow(C * dt, 2) * pot * 2 + 4 * data[i * GRID_SZ + j] - olddata[i * GRID_SZ + j] 
+								* (2 - K * dt)) 
+								/ (2 + K * dt);
+				}
+			}
+		}
+}
 // WITHOMP PART END
 
 // WITHOMP PART
-double *process_withomp() {
+double *process_withompusingfor() {
 		double start = omp_get_wtime();
 		
 		int i, j;
@@ -147,8 +233,8 @@ double *process_withomp() {
 		
 		#pragma omp barrier
 			 
-		for(i = 0; i < 20; i++) {
-			sequential_update_withomp( data, olddata, newdata, C, K, dt);
+		for(i = 0; i < TIMES; i++) {
+			sequential_update_withompusingfor( data, olddata, newdata, C, K, dt);
 			tmp = olddata;
 			olddata = data;
 			data = newdata;
@@ -160,7 +246,7 @@ double *process_withomp() {
 		printf("with omp spend: %f\n",end-start);
 		return data;
 }
-void sequential_update_withomp(double *data, double *olddata, double *newdata, double C, double K, double dt ) {
+void sequential_update_withompusingfor(double *data, double *olddata, double *newdata, double C, double K, double dt ) {
 		int i, j;
 		double pot;
 		#pragma omp parallel for private(i,j,pot) schedule(auto)
@@ -215,7 +301,7 @@ double *process_withoutomp() {
 				olddata[i] = data[i];
 		}
 
-		for(i = 0; i < 20; i++){
+		for(i = 0; i < TIMES; i++){
 				sequential_update_withoutomp( data, olddata, newdata, C, K, dt);
 				tmp = olddata;
 				olddata = data;
--- unit_test.h
+++ unit_test.h
@@ -3,13 +3,19 @@
 #include <omp.h>
 #include <math.h>
 
-#define GRID_SZ 3000
+#define GRID_SZ 1000
 #define ARR_SZ GRID_SZ * GRID_SZ
 #define PEAK_SZ 31
+#define TIMES 30
 
-double *process_withomp();
-double *process_withomp1();
+double *process_withompusingtask();
+double *process_withompusingunrollingfor();
+double *process_withompusingfor();
+
 double *process_withoutomp();
-void sequential_update_withomp(double *data, double *olddata, double *newdata, double, double, double );
-void sequential_update_withomp1(double *data, double *olddata, double *newdata, double, double, double );
+
+void sequential_update_withompusingtask(double *data, double *olddata, double *newdata, double, double, double );
+void sequential_update_withompusingunrollingfor(double *data, double *olddata, double *newdata, double, double, double );
+void sequential_update_withompusingfor(double *data, double *olddata, double *newdata, double, double, double );
+
 void sequential_update_withoutomp(double *data, double *olddata, double *newdata, double C, double K, double dt );
