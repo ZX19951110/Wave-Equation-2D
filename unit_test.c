987cd9cd31d6567102154b8a223cc4b33a07b693
langker <langker@aliyun.com>
2018-01-10 03:01:32 +0100

    use task to speed the init part

--- unit_test.c
+++ unit_test.c
@@ -38,57 +38,65 @@ double *process_withompusingtask() {
 		
 		#pragma omp parallel
 		{
-			#pragma omp for private(i) schedule(auto)
-			for(i = 0; i < ARR_SZ; i++) {
-					data[i] = 1.0;
-			}
-			
-			#pragma omp for private(i,j) schedule(auto)
-			for(i = 0; i < PEAK_SZ; i++){
-				linspace[i] = -1.0 + delta * i;
-				for(j = 0; j < PEAK_SZ; j++) {
-					x[i][j] = linspace[i];
-				}
-			}
-			
-			
-			#pragma omp for private(i,j) schedule(auto)
-			for(i = 0; i < PEAK_SZ; i++){
-				for(j = 0; j < PEAK_SZ; j++){
-					data[(i+20)*GRID_SZ+j+20] += h * exp( -5 * (pow(x[i][j], 2 ) + pow(x[j][i], 2 )));
-				}
-			}
-			
-			#pragma omp for private(i) schedule(auto)
-			for(i = 0; i < ARR_SZ; i++){
-				olddata[i] = data[i];
-			}
-			#pragma omp single  		
-			for(i = 0; i < TIMES; i++) {
-				int n, m;
-				double pot;
+			#pragma omp single
+			{
 				#pragma omp task
-				for( n = 0; n < GRID_SZ; n++) {
-					for( m = 0; m < GRID_SZ; m++) {
-						pot = data[(n+1 >= GRID_SZ ? n : n+1)*GRID_SZ+m]+
-												data[(n-1 < 0 ? 0 : n-1)*GRID_SZ+m]+
-												data[(m+1 >= GRID_SZ ? m : m+1)+n*GRID_SZ]+
-												data[(m-1 < 0 ? 0 : m-1)+n*GRID_SZ]
-												-4*data[n*GRID_SZ+m];
-						newdata[n * GRID_SZ + m] = 
-								(pow(C * dt, 2) * pot * 2 + 4 * data[n * GRID_SZ + m] - olddata[n * GRID_SZ + m] 
-														* (2 - K * dt)) 
-														/ (2 + K * dt);
-									
+				for(i = 0; i < ARR_SZ; i++) {
+					data[i] = 1.0;
+				}
+				
+				#pragma omp taskwait
+				
+				#pragma omp task
+				for(i = 0; i < PEAK_SZ; i++){
+					linspace[i] = -1.0 + delta * i;
+					for(j = 0; j < PEAK_SZ; j++) {
+						x[i][j] = linspace[i];
+					}
+				}
+				
+				#pragma omp taskwait
+				
+				#pragma omp task
+				for(i = 0; i < PEAK_SZ; i++){
+					for(j = 0; j < PEAK_SZ; j++){
+						data[(i+20)*GRID_SZ+j+20] += h * exp( -5 * (pow(x[i][j], 2 ) + pow(x[j][i], 2 )));
 					}
 				}
 				#pragma omp taskwait
-				tmp = olddata;
-				olddata = data;
-				data = newdata;
-				newdata = tmp;
+				
+				#pragma omp task
+				for(i = 0; i < ARR_SZ; i++){
+					olddata[i] = data[i];
+				}
+				
 			}
 		}
+		for(i = 0; i < TIMES; i++) {
+			int n, m;
+			double pot;
+			#pragma omp parallel for private(n,m,pot) schedule(auto)
+			for( n = 0; n < GRID_SZ; n++) {
+				for( m = 0; m < GRID_SZ; m++) {
+					pot = data[(n+1 >= GRID_SZ ? n : n+1)*GRID_SZ+m]+
+						data[(n-1 < 0 ? 0 : n-1)*GRID_SZ+m]+
+						data[(m+1 >= GRID_SZ ? m : m+1)+n*GRID_SZ]+
+						data[(m-1 < 0 ? 0 : m-1)+n*GRID_SZ]
+						-4*data[n*GRID_SZ+m];
+					newdata[n * GRID_SZ + m] = 
+									(pow(C * dt, 2) * pot * 2 + 4 * data[n * GRID_SZ + m] - olddata[n * GRID_SZ + m] 
+															* (2 - K * dt)) 
+															/ (2 + K * dt);
+										
+				}
+			}
+			tmp = olddata;
+			olddata = data;
+			data = newdata;
+			newdata = tmp;
+		}
+			
+		
 		double end = omp_get_wtime();
 		printf("with omp spend: %f\n",end-start);
 		return data;
