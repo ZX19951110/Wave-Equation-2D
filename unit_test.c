fb704eb8779b223963da587708187b75a61f49b5
langker <langker@aliyun.com>
2018-01-23 16:32:06 +0100

    can edit the GRID_SZ when run the program

--- unit_test.c
+++ unit_test.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include "unit_test.h"
 
+int sz;
+int arr_sz;
 int main(int argc, char *argv[]) {
+	
+	sz = (argc==2?atoi(argv[1]):GRID_SZ);
+	sz = sz>=100?sz:GRID_SZ;
+	arr_sz = sz * sz;
 	
 	double *rightdata= process_withoutomp();
 	double *testdatausingtask= process_withompusingtask();
 	double *testdatausingfor= process_withompusingfor();
 	
-	for(int i = 0; i < GRID_SZ; i++) {
-		for(int j = 0; j < GRID_SZ; j++) {
-			if (rightdata[i*GRID_SZ+j]!=testdatausingtask[i*GRID_SZ+j]) {
+	for(int i = 0; i < sz; i++) {
+		for(int j = 0; j < sz; j++) {
+			if (rightdata[i*sz+j]!=testdatausingtask[i*sz+j]) {
 				printf("wrong task");
 				return 0;
 			}
 		}
 	}
-	for(int i = 0; i < GRID_SZ; i++) {
-		for(int j = 0; j < GRID_SZ; j++) {
-			if (rightdata[i*GRID_SZ+j]!=testdatausingfor[i*GRID_SZ+j]) {
+	for(int i = 0; i < sz; i++) {
+		for(int j = 0; j < sz; j++) {
+			if (rightdata[i*sz+j]!=testdatausingfor[i*sz+j]) {
 				printf("wrong for");
 				return 0;
 			}
@@ -32,16 +38,16 @@ double *process_withompusingtask() {
 		double dt = 0.04, C = 16, K = 0.1, h = 6;
 		double *data, *olddata, *newdata, *tmp;
 		double x[PEAK_SZ][PEAK_SZ], linspace[PEAK_SZ], delta = 2.0/(PEAK_SZ-1.0);
-		data = (double*)malloc(sizeof(double)*ARR_SZ);
-		olddata = (double*)malloc(sizeof(double)*ARR_SZ);
-		newdata = (double*)malloc(sizeof(double)*ARR_SZ);
+		data = (double*)malloc(sizeof(double)*arr_sz);
+		olddata = (double*)malloc(sizeof(double)*arr_sz);
+		newdata = (double*)malloc(sizeof(double)*arr_sz);
 		
 		#pragma omp parallel
 		{
 			#pragma omp single
 			{
 				#pragma omp task
-				for(i = 0; i < ARR_SZ; i++) {
+				for(i = 0; i < arr_sz; i++) {
 					olddata[i] = data[i] = 1.0;
 				}
 				
@@ -60,7 +66,7 @@ double *process_withompusingtask() {
 				#pragma omp task
 				for(i = 0; i < PEAK_SZ; i++){
 					for(j = 0; j < PEAK_SZ; j++){
-						olddata[(i+20)*GRID_SZ+j+20] = data[(i+20)*GRID_SZ+j+20] += h * exp( -5 * (pow(x[i][j], 2 ) + pow(x[j][i], 2 )));
+						olddata[(i+20)*sz+j+20] = data[(i+20)*sz+j+20] += h * exp( -5 * (pow(x[i][j], 2 ) + pow(x[j][i], 2 )));
 					}
 				}
 				#pragma omp taskwait
@@ -71,15 +77,15 @@ double *process_withompusingtask() {
 			int n, m;
 			double pot;
 			#pragma omp parallel for private(n,m,pot) schedule(auto)
-			for( n = 0; n < GRID_SZ; n++) {
-				for( m = 0; m < GRID_SZ; m++) {
-					pot = data[(n+1 >= GRID_SZ ? n : n+1)*GRID_SZ+m]+
-						data[(n-1 < 0 ? 0 : n-1)*GRID_SZ+m]+
-						data[(m+1 >= GRID_SZ ? m : m+1)+n*GRID_SZ]+
-						data[(m-1 < 0 ? 0 : m-1)+n*GRID_SZ]
-						-4*data[n*GRID_SZ+m];
-					newdata[n * GRID_SZ + m] = 
-									(pow(C * dt, 2) * pot * 2 + 4 * data[n * GRID_SZ + m] - olddata[n * GRID_SZ + m] 
+			for( n = 0; n < sz; n++) {
+				for( m = 0; m < sz; m++) {
+					pot = data[(n+1 >= sz ? n : n+1)*sz+m]+
+						data[(n-1 < 0 ? 0 : n-1)*sz+m]+
+						data[(m+1 >= sz ? m : m+1)+n*sz]+
+						data[(m-1 < 0 ? 0 : m-1)+n*sz]
+						-4*data[n*sz+m];
+					newdata[n * sz + m] = 
+									(pow(C * dt, 2) * pot * 2 + 4 * data[n * sz + m] - olddata[n * sz + m] 
 															* (2 - K * dt)) 
 															/ (2 + K * dt);
 										
@@ -93,7 +99,7 @@ double *process_withompusingtask() {
 			
 		
 		double end = omp_get_wtime();
-		printf("with omp spend: %f\n",end-start);
+		printf("with omp task spend: %f\n",end-start);
 		return data;
 }
 double *process_withompusingfor() {
@@ -103,14 +109,14 @@ double *process_withompusingfor() {
 		double dt = 0.04, C = 16, K = 0.1, h = 6;
 		double *data, *olddata, *newdata, *tmp;
 		double x[PEAK_SZ][PEAK_SZ], linspace[PEAK_SZ], delta = 2.0/(PEAK_SZ-1.0);
-		data = (double*)malloc(sizeof(double)*ARR_SZ);
-		olddata = (double*)malloc(sizeof(double)*ARR_SZ);
-		newdata = (double*)malloc(sizeof(double)*ARR_SZ);
+		data = (double*)malloc(sizeof(double)*arr_sz);
+		olddata = (double*)malloc(sizeof(double)*arr_sz);
+		newdata = (double*)malloc(sizeof(double)*arr_sz);
 		
 		#pragma omp parallel
 		{
 			#pragma omp for private(i) schedule(auto)
-			for(i = 0; i < ARR_SZ; i++) {
+			for(i = 0; i < arr_sz; i++) {
 				olddata[i] = data[i] = 1.0;
 			}
 			
@@ -128,7 +134,7 @@ double *process_withompusingfor() {
 			#pragma omp for private(i,j) schedule(auto)
 			for(i = 0; i < PEAK_SZ; i++){
 				for(j = 0; j < PEAK_SZ; j++){
-					olddata[(i+20)*GRID_SZ+j+20] = data[(i+20)*GRID_SZ+j+20] += h * exp( -5 * (pow(x[i][j], 2 ) + pow(x[j][i], 2 )));
+					olddata[(i+20)*sz+j+20] = data[(i+20)*sz+j+20] += h * exp( -5 * (pow(x[i][j], 2 ) + pow(x[j][i], 2 )));
 				}
 			}
 		}
@@ -136,15 +142,15 @@ double *process_withompusingfor() {
 			int n, m;
 			double pot;
 			#pragma omp parallel for private(n,m,pot) schedule(auto)
-			for( n = 0; n < GRID_SZ; n++) {
-				for( m = 0; m < GRID_SZ; m++) {
-					pot = data[(n+1 >= GRID_SZ ? n : n+1)*GRID_SZ+m]+
-													data[(n-1 < 0 ? 0 : n-1)*GRID_SZ+m]+
-													data[(m+1 >= GRID_SZ ? m : m+1)+n*GRID_SZ]+
-													data[(m-1 < 0 ? 0 : m-1)+n*GRID_SZ]
-													-4*data[n*GRID_SZ+m];
-					newdata[n * GRID_SZ + m] = 
-									(pow(C * dt, 2) * pot * 2 + 4 * data[n * GRID_SZ + m] - olddata[n * GRID_SZ + m] 
+			for( n = 0; n < sz; n++) {
+				for( m = 0; m < sz; m++) {
+					pot = data[(n+1 >= sz ? n : n+1)*sz+m]+
+													data[(n-1 < 0 ? 0 : n-1)*sz+m]+
+													data[(m+1 >= sz ? m : m+1)+n*sz]+
+													data[(m-1 < 0 ? 0 : m-1)+n*sz]
+													-4*data[n*sz+m];
+					newdata[n * sz + m] = 
+									(pow(C * dt, 2) * pot * 2 + 4 * data[n * sz + m] - olddata[n * sz + m] 
 															* (2 - K * dt)) 
 															/ (2 + K * dt);
 										
@@ -158,7 +164,7 @@ double *process_withompusingfor() {
 		}
 		
 		double end = omp_get_wtime();
-		printf("with omp spend: %f\n",end-start);
+		printf("with omp for spend: %f\n",end-start);
 		return data;
 }
 
@@ -169,11 +175,11 @@ double *process_withoutomp() {
 		double dt = 0.04, C = 16, K = 0.1, h = 6;
 		double *data, *olddata, *newdata, *tmp;
 		double x[PEAK_SZ][PEAK_SZ], linspace[PEAK_SZ], delta = 2.0/(PEAK_SZ-1.0);
-		data = (double*)malloc(sizeof(double)*ARR_SZ);
-		olddata = (double*)malloc(sizeof(double)*ARR_SZ);
-		newdata = (double*)malloc(sizeof(double)*ARR_SZ);
+		data = (double*)malloc(sizeof(double)*arr_sz);
+		olddata = (double*)malloc(sizeof(double)*arr_sz);
+		newdata = (double*)malloc(sizeof(double)*arr_sz);
 	
-		for(i = 0; i < ARR_SZ; i++){
+		for(i = 0; i < arr_sz; i++){
 			data[i] = 1.0;
 		}
 	 
@@ -189,11 +195,11 @@ double *process_withoutomp() {
 		
 		for(i = 0; i < PEAK_SZ; i++){
 			for(j = 0; j < PEAK_SZ; j++){
-				data[(i+20)*GRID_SZ+j+20] += h * exp( -5 * (pow(x[i][j], 2 ) + pow(x[j][i], 2 )));
+				data[(i+20)*sz+j+20] += h * exp( -5 * (pow(x[i][j], 2 ) + pow(x[j][i], 2 )));
 			}
 		}
 		
-		for(i = 0; i < ARR_SZ; i++){
+		for(i = 0; i < arr_sz; i++){
 			olddata[i] = data[i];
 		}
 
@@ -212,19 +218,19 @@ double *process_withoutomp() {
 void sequential_update_withoutomp(double *data, double *olddata, double *newdata, double C, double K, double dt ){
 		int i, j, add_i, sub_i, add_j, sub_j;
 		double pot;
-		for( i = 0; i < GRID_SZ; i++){
-				for( j = 0; j < GRID_SZ; j++){
-						add_i = i+1 >= GRID_SZ ? i : i+1;
-						add_j = j+1 >= GRID_SZ ? j : j+1;
+		for( i = 0; i < sz; i++){
+				for( j = 0; j < sz; j++){
+						add_i = i+1 >= sz ? i : i+1;
+						add_j = j+1 >= sz ? j : j+1;
 						sub_i = i-1 < 0 ? 0 : i-1;
 						sub_j = j-1 < 0 ? 0 : j-1;
-						pot = data[add_i*GRID_SZ+j]+
-									data[sub_i*GRID_SZ+j]+
-									data[add_j+i*GRID_SZ]+
-									data[sub_j+i*GRID_SZ]-
-									4*data[i*GRID_SZ+j];
-						newdata[i * GRID_SZ + j] = 
-								( pow(C * dt, 2) * pot * 2 + 4 * data[i * GRID_SZ + j] - olddata[i * GRID_SZ + j] *(2 - K * dt) ) / (2 + K * dt);
+						pot = data[add_i*sz+j]+
+									data[sub_i*sz+j]+
+									data[add_j+i*sz]+
+									data[sub_j+i*sz]-
+									4*data[i*sz+j];
+						newdata[i * sz + j] = 
+								( pow(C * dt, 2) * pot * 2 + 4 * data[i * sz + j] - olddata[i * sz + j] *(2 - K * dt) ) / (2 + K * dt);
 				}
 		}
 }
